adb5c98d4f8de6524678154703ee8897eaaa07e1
Leveton <mleveton@gmail.com>
2015-03-01 22:01:50 -0500

    initial commit

--- MELBrowser/ViewController.m
+++ MELBrowser/ViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate, WKNavigationDelegate>
+
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, weak) IBOutlet UIView *barView;
+@property (nonatomic, weak) IBOutlet UITextField *urlField;
+
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *backButton;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *forwardButton;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *reloadButton;
+@property (nonatomic, weak) IBOutlet UIProgressView *progressView;
 
 @end
 
@@ -16,12 +26,72 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.barView setFrame:CGRectMake(0, 0, self.view.frame.size.width, 30)];
+    
+    self.webView = [[WKWebView alloc]initWithFrame:CGRectZero];
+    self.webView.navigationDelegate = self;
+    [self.progressView.layer setZPosition:2];
+    [self.webView.layer setZPosition:1];
+    [self.view addSubview:self.webView];
+    
+    self.webView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.webView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:1 constant:-44];
+    
+    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:self.webView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:1 constant:0];
+    
+    [self.view addConstraint:height];
+    [self.view addConstraint:width];
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.appcoda.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+   [self.barView setFrame:CGRectMake(0, 0, size.width, 30)];
+}
+
+#pragma mark - WKNavigationDelegate
+
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error
+{
+    if (error)
+    {
+       UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil]];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
+{
+    [self.progressView setProgress:0.0 animated:NO];
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if ([self.urlField isFirstResponder])
+    {
+        [self.urlField resignFirstResponder];
+    }
+    
+    NSURL *url = [NSURL URLWithString:self.urlField.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    
+    return NO;
 }
 
 @end
