9e7cca5874bf1aacdaab062a79ecd6a31fcd32a1
Leveton <mleveton@gmail.com>
2015-03-01 20:41:23 -0500

    Initial Commit

---
+++ MELBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MELBrowser
+//
+//  Created by Mike Leveton on 3/1/15.
+//  Copyright (c) 2015 MEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MELBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MELBrowser
+//
+//  Created by Mike Leveton on 3/1/15.
+//  Copyright (c) 2015 MEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
